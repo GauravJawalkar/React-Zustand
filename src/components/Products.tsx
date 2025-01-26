3b0743a9a99b8a46cfe9ab0830d1e4472b7f5f64
GauravJawalkar <gauravjawalkar8@gmail.com>
2025-01-26 20:46:51 +0530

    first commit

---
+++ src/components/Products.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+
+interface ProductListType {
+    id: number,
+    name: string,
+    description: string
+}
+
+const Products = () => {
+    const productsList: ProductListType[] = [
+        {
+            id: Math.ceil(Math.random() * 100000 + 1),
+            name: "Iphone 16 pro max",
+            description: "This is a description for product of iphone 16 pro max"
+        },
+        {
+            id: Math.ceil(Math.random() * 100000 + 1),
+            name: "Iphone 15 pro max",
+            description: "This is a description for product of iphone 15 pro max"
+        },
+        {
+            id: Math.ceil(Math.random() * 100000 + 1),
+            name: "Iphone 14 pro max",
+            description: "This is a description for product of iphone 14 pro max"
+        },
+        {
+            id: Math.ceil(Math.random() * 100000 + 1),
+            name: "Iphone 13 pro max",
+            description: "This is a description for product of iphone 13 pro max"
+        },
+        {
+            id: Math.ceil(Math.random() * 100000 + 1),
+            name: "Iphone 12 pro max",
+            description: "This is a description for product of iphone 12 pro max"
+        }
+    ]
+    return (
+        <div>
+            {
+                productsList.map(({ id, name, description }) => {
+                    return (
+                        <div key={id}>
+                            <div className='my-5'>
+                                <h1 className='text-3xl'>{name}</h1>
+                                <p className='text-gray-400'>{description}</p>
+                            </div>
+                            <button className='bg-gray-800 px-3 py-2 rounded'>Add Product</button>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
+}
+
+export default Products
